ea8ad1f925a35f0b4816baa13165e4f4c7a52f46
AndyVera <andy.vera13@csu.fullerton.edu>
2022-09-20 16:13:52 -0700

    Initial Commit

---
+++ Live Demo - UI Bar, TabBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Live Demo - UI Bar, TabBar
+//
+//  Created by Andy Vera on 9/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
